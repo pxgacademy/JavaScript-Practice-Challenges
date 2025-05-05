d8fb482fc9025997f60b519d18f96ba46146b7d6
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-05 18:14:18 +0600

    Practice task 03 completed

---
+++ Chapter-02-Variables/practice-003/practice-003.js
@@ -0,0 +1,15 @@
+const age = 120,
+  booksToRead = 100,
+  wakeUpTime = 5,
+  wastedTime = 3,
+  borrow = 200000,
+  goals = 2.7,
+  pillow = 1;
+
+console.log(age);
+console.log(booksToRead);
+console.log(wakeUpTime);
+console.log(wastedTime);
+console.log(borrow);
+console.log(goals);
+console.log(pillow);
