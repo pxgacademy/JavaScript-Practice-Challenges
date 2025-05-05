6e935c85e7a9a5a5a6700f39f31f1fa81c40c0ac
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-05 19:21:44 +0600

    Practice task 06 completed

---
+++ Chapter-02-Variables/practice-006/practice-006.js
@@ -0,0 +1,84 @@
+// 01 ___________________________________
+{
+  const income = 100,
+    spent = 40;
+  const left = income - spent;
+  console.log(left);
+}
+
+// 02 ___________________________________
+{
+  const pencil = 10,
+    usePerWeek = 2;
+  const weeks = pencil / usePerWeek;
+  console.log(weeks);
+}
+
+// 03 ___________________________________
+{
+  const taka = 60,
+    spendPerTime = 15;
+  const times = taka / spendPerTime;
+  console.log(times);
+}
+
+// 04 ___________________________________
+{
+  const hello = "Hello",
+    world = "World";
+  const helloWorld = hello + world;
+  console.log(helloWorld);
+}
+
+// 05 ___________________________________
+{
+  const rice1 = 153,
+    rice2 = 261;
+  const totalRice = rice1 + rice2;
+  console.log(totalRice);
+}
+
+// 06 ___________________________________
+{
+  const taka = 500,
+    spendPerTime = 75;
+  const times = taka / spendPerTime;
+  console.log(times);
+}
+
+// 07 ___________________________________
+{
+  const num1 = 8,
+    num2 = 3;
+  const remainder = num1 % num2;
+  const quotient = (num1 - remainder) / num2;
+  const quotient2 = Math.floor(num1 / num2);
+  console.log(remainder);
+  console.log(quotient);
+  console.log(quotient2);
+}
+
+// 08 ___________________________________
+{
+  const num1 = 50,
+    num2 = 9;
+  const remainder = num1 % num2;
+  console.log(remainder);
+}
+
+// 09 ___________________________________
+{
+  const bangla = "Bangla",
+    desh = "desh";
+  const bangladesh = bangla + desh;
+  console.log(bangladesh);
+}
+
+// 10 ___________________________________
+{
+  const totalRice = 480,
+    dailyNeeds = 4;
+  const days = totalRice / dailyNeeds;
+  const months = days / 30;
+  console.log(days, months);
+}
