3bb9beb3f7f610df3e238f69627618f56f39f3df
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-05 20:27:25 +0600

    Practice task 09 completed

---
+++ Chapter-03-Data-Types/practice-009-Comments/practice-009.js
@@ -0,0 +1,12 @@
+// this is a comment, this is a test-cricket score. ind vs ban
+const score = 532;
+
+// this is a radius of my cricket ball
+// this is not fixed radius, sometimes it can change based on the match type.
+// like: white ball can be 6, red ball can be 7
+const radius = 7;
+
+/*
+Panjabi is my favorite dress. I love to ware panjabi when I go to any occasion or visit any new or old places. I have many panjabis. I love to ware black panjabi. Basically I love to ware any deep color panjabi.
+*/
+const dress = "Panjabi";
