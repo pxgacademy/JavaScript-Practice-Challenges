a9556f77d9909b4d1e5de11a13006b08bb3dc318
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-05 19:42:24 +0600

    Practice task 08 completed

--- Chapter-03-Data-Types/practice-007-Variable-Type/practice-007.js
+++ Chapter-03-Data-Types/practice-007-Variable-Type/practice-007.js
@@ -1,17 +1,23 @@
+// 01 ___________________________________
 const price = 555;
 console.log(typeof price);
 
+// 02 ___________________________________
 const learning = true;
 console.log(typeof learning);
 
+// 03 ___________________________________
 const megaCity = "Dhaka";
 console.log(typeof megaCity);
 
+// 04 ___________________________________
 const temperature = 102.51;
 console.log(typeof temperature);
 
+// 05 ___________________________________
 const isHungry = true;
 console.log(typeof isHungry);
 
+// 06 ___________________________________
 const isRich = true;
 console.log(typeof isRich);
---
+++ Chapter-03-Data-Types/practice-008-let-variable/practice-008.js
@@ -0,0 +1,55 @@
+// 01 ___________________________________
+{
+  let age = 120;
+  console.log(age);
+  age = 90;
+  console.log(age);
+}
+
+// 02 ___________________________________
+{
+  let pocketMoney = 500;
+  console.log(pocketMoney);
+  pocketMoney = 150;
+  console.log(pocketMoney);
+}
+
+// 03 ___________________________________
+{
+  let studyTime = 4;
+  console.log(studyTime);
+  studyTime = 0;
+  console.log(studyTime);
+}
+
+// 04 ___________________________________
+{
+  let friendsCount = 3;
+  console.log(friendsCount);
+  friendsCount = 1;
+  console.log(friendsCount);
+}
+
+// 05 ___________________________________
+{
+  let price = 25000;
+  console.log(price);
+  price = 12000;
+  console.log(price);
+}
+
+// 06 ___________________________________
+{
+  let chairs = 40;
+  console.log(chairs);
+  chairs = 34;
+  console.log(chairs);
+}
+
+// 07 ___________________________________
+{
+  let playTime = 3;
+  console.log(playTime);
+  playTime = 0;
+  console.log(playTime);
+}
