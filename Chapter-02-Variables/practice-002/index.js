9a0d0e4b888c3f6db6c4d6d076e4bc7f9b15a5f6
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-05 18:00:54 +0600

    Practice task 02 completed

---
+++ Chapter-02-Variables/practice-002/index.js
@@ -0,0 +1,20 @@
+const birthYear = 1850;
+const siblings = 3;
+const students = 1000;
+const marks = 520;
+const rooms = 4;
+const studyTime = 12;
+const number = 620;
+
+{
+  // you can declare all variables with one const
+
+  const birthYear = 1850,
+    siblings = 3,
+    students = 1000,
+    marks = 500,
+    rooms = 4,
+    studyTime = 12,
+    number = 620;
+  // like that
+}
