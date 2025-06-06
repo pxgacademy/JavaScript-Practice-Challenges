c57285b3d3ec3a312a17b991c1a507148f060098
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-06-06 18:08:46 +0600

    completed practice no 11

---
+++ Chapter-03-Data-Types/practice-011-parseInt-parseFloat-toFixed/practice-011.js
@@ -0,0 +1,49 @@
+{
+  // practice #01
+  const num1 = "20";
+  const num2 = 10;
+
+  const total = parseInt(num1) + num2;
+  console.log(total);
+}
+{
+  // practice #02
+  const decimal = 3.14159;
+  const integer = parseInt(decimal);
+  console.log(integer);
+}
+{
+  // practice #03
+  const string = "premikBoy";
+  const tryToMakeNumber = parseInt(string);
+  console.log(tryToMakeNumber);
+}
+{
+  // practice #04
+  const num1 = 3.456;
+  const num2 = 2.789;
+  const total = num1 + num2;
+  const output = total.toFixed(2);
+  console.log(output);
+}
+{
+  // practice #05
+  const num1 = "56.78";
+  const num2 = "12.34";
+  const total = parseFloat(num1) + parseFloat(num2);
+  console.log(total);
+}
+{
+  // practice #06
+  const num1 = "10.5678";
+  const num2 = "5.4321";
+  const total = parseFloat(num1) + parseFloat(num2);
+  const output = total.toFixed(1);
+  console.log(output);
+}
+{
+  // practice #07
+  const num = 42.45689754;
+  const output = num.toFixed(3);
+  console.log(output);
+}
