2aee9516c90df7e7fbc6778a58cee226e94e226d
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-06-06 18:28:53 +0600

    completed practice no 12

--- Chapter-03-Data-Types/practice-010-variable-name/practice-010.js
+++ Chapter-03-Data-Types/practice-010-variable-name/practice-010.js
@@ -1,17 +1,25 @@
-//
 
+// practice #01
 const favoriteFood = "Burger";
 
+// practice #02
 const city = "Chattogram";
 const City = "Dhaka";
 console.log(city, City);
 
+// practice #03
 const userName = "Johan";
 const username = "Jovan";
 console.log(userName, username);
 
+// practice #04
 const myHomeAddress = 'Hathazari, Chattogram'
 
+// practice #05
 const super = 24; // super is a keyword. so the variable will not work.
+
+// practice #06
 const class = 18; // class is also a keyword.
+
+// practice #07
 const 2ndPosition = 'Sakib'; // first letter should not be numeric
---
+++ Chapter-04-Comparison/practice-012/practice-012.js
@@ -0,0 +1,49 @@
+{
+  // practice #01
+  const iTook = 7;
+  const myBrotherTook = 5;
+  const output = iTook > myBrotherTook;
+  console.log(output);
+}
+{
+  // practice #02
+  const class1 = 45;
+  const class2 = 50;
+  const output = class1 > class2;
+  console.log(output);
+}
+{
+  // practice #03
+  const iGot = 75;
+  const myFriendGot = 75;
+  const output = iGot == myFriendGot;
+  console.log(output);
+}
+{
+  // practice #04
+  const target = 100;
+  const present = 95;
+  const output = target <= present;
+  console.log(output);
+}
+{
+  // practice #05
+  const num1 = 10;
+  const num2 = 7;
+  const output = num1 != num2;
+  console.log(output);
+}
+{
+  // practice #06
+  const num1 = 20;
+  const num2 = 15;
+  const output = num1 <= num2;
+  console.log(output);
+}
+{
+  // practice #07
+  const num1 = 45;
+  const num2 = 97;
+  const output = num1 >= num2;
+  console.log(output);
+}
