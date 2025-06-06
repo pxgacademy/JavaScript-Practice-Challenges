b65aca9ca3e5237de6900bc5757d93db03a153b8
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-06-06 17:39:29 +0600

    completed practice no 10

---
+++ Chapter-03-Data-Types/practice-010-variable-name/practice-010.js
@@ -0,0 +1,17 @@
+//
+
+const favoriteFood = "Burger";
+
+const city = "Chattogram";
+const City = "Dhaka";
+console.log(city, City);
+
+const userName = "Johan";
+const username = "Jovan";
+console.log(userName, username);
+
+const myHomeAddress = 'Hathazari, Chattogram'
+
+const super = 24; // super is a keyword. so the variable will not work.
+const class = 18; // class is also a keyword.
+const 2ndPosition = 'Sakib'; // first letter should not be numeric
